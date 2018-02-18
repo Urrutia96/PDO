b722a1c6bb52fad5ef62479acc8bef18068b7194
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2018-02-18 14:28:17 -0600

    Add files via upload

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -4,17 +4,25 @@
 */
 require_once('Connector.php');
 class Usuario{
+	//Se declaran los atributos de la clase
 	private $id;
 	private $nombre;
 	private $edad;
 	private $telefono;
 	private $correo;
+	//Lo utilizaremos para guardar los resultado de una busqueda
 	private $result = null;
-
+	//Se guarda la coneccion en esta variable
 	private $conn = null;
+
+	//Para no instanciar tanto la clase de conexion lo haremos en el constructor
+	//los parametros los definimos como null para que no sea obligacion pasarle datos
 	function __construct($id=null,$nombre=null,$edad=null,$telefono=null,$correo=null){
+		//Instanciamos la clase de conexion
 		$model = new Connector();
+		//Le asignamos la coneccion a la variable mediante un metodo de la case de conexion
 		$this->conn = $model->getConexion();
+		//Si el contructor resive algun parametro se le asigna el valor al atributo correspondiente
 		$this->id = $id;
 		$this->nombre = $nombre;
 		$this->edad = $edad;
@@ -55,31 +63,45 @@ class Usuario{
 		return $this->correo;
 	}
 
-
+	//Para poder usar este metodo, anteriormente tenemos que pasar todos los datos por medio de los metodos setter
 	public function guardar(){
+		//Se crea la sentencia sql dejandole indicado que se le pasaran parametros luego, ejemplo ":nombre"
 		$sql = "INSERT INTO usuario (nombre, edad, telefono, correo) VALUES (:nombre, :edad, :telefono, :correo)";
+		//Se prepara la conexion para que reciba la sentencia sql
 		$statement = $this->conn->prepare($sql);
+		//Una ves preparada, solamente le pasamos los parametros con la funcion bindParam
 		$statement->bindParam(':nombre', $this->nombre);
 		$statement->bindParam(':edad', $this->edad);
 		$statement->bindParam(':telefono', $this->telefono);
 		$statement->bindParam(':correo', $this->correo);
-		if($statement){
-			$statement->execute();
+		//Comprobamos si no hay problemas
+		//sino los hay retornamos true indicando que todo salio bien 
+		if($statement->execute()){
 			return true;
 		}else{
 			return false;			
 		}
 	}
-
+	
+	//Para usar esta funcion solo tenemos que settear el valor del id 
 	public function buscarPorId(){
+		////Se crea la sentencia sql dejandole indicado que se le pasaran parametros luego, ejemplo ":id"
 		$sql = "SELECT  nombre, edad, telefono, correo FROM usuario WHERE id = :id";
 		$statement = $this->conn->prepare($sql);
 		$statement->bindParam(':id',$this->id);
+		//Se ejecuta la sentencia
 		$statement->execute();
+		//con la funcion fetch lo que hacemos es que en una variable guardamos un arreglo que lo pordemos usar asi,ejemplo "$result['nombre']"
 		$this->result = $statement->fetch();
+		//Retornamos un objeto del tipo de la clase Usuario
+		//Esto para poder hacer uso de los datos, sera de esta forma
+		//$usuari->setId($id);
+		//$resultado = $usuario->buscarPorId();
+		//$resultado->getNombre():
 		return new self($this->id,$this->result['nombre'],$this->result['edad'],$this->result['telefono'],$this->result['correo']);	
 	}
 
+	//Para usar este metodo solamente lo tenemos que llamar
 	public function buscarTodo(){
 		$sql = "SELECT id, nombre, edad, telefono, correo FROM usuario";
 		$statement = $this->conn->prepare($sql);
@@ -88,8 +110,10 @@ class Usuario{
 		if ($this->result) {
 			return $this->result;
 		}
+		//para poder usar los datos retornado tendremos que hacer uso de un foreach($result as $user){ $user['nombre']} 
 	}
 
+	//Solo se pasa el valor del id por medio de setId() y luego llamar la funcion 
 	public function eliminar(){
 		$sql = "DELETE FROM usuario WHERE id = :id";
 		$statement = $this->conn->prepare($sql);
@@ -101,6 +125,8 @@ class Usuario{
 		}
 	}
 
+	//Se les asignan los valores por medio de los setter y luego se llama la funcion 
+	//retorna true si se ejecuta y false si hay alguna falla
 	public function modificar(){
 		$sql = "UPDATE usuario SET nombre = :nombre, edad = :edad, telefono = :telefono, correo = :correo WHERE id = :id";
 		$statement = $this->conn->prepare($sql);
@@ -116,6 +142,7 @@ class Usuario{
 		}
 	}
 
+	//En ese metodo igualamos a null los objetos que hemos utilizado, asi para que no consuman recursos
 	function __destruct(){
 		$this->conn = null;
 		$this->result = null;
