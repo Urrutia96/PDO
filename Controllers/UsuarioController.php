7324b932d7168373b39953650e1a61a00c385166
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2018-02-18 14:29:20 -0600

    Add files via upload

--- Controllers/UsuarioController.php
+++ Controllers/UsuarioController.php
@@ -1,26 +1,31 @@
 <?php 
+/**
+* @author Santos Osmin Urrutia Iraheta
+*/
 include('Models/Usuario.php');
 	class UsuarioController{
-
 		private $id;
-		public $nombre;
-		public $edad;
-		public $telefono;
-		public $correo;
+		//Se declara la variable en la que vamos a guardar la instancia de
+		//la clase modelo Usuario
 		private $usuario = null;
+		//Declaramos la instancia a la clase usuario desde la creacion de la instancia dela clase UsuarioController
 		function __construct(){
 			$this->usuario = new Usuario(); 
 		}
+
+		//Solamente se recibe el id, hace la peticion al modelo y retorna un objeto de tipo Usuario
 		public function buscar($id){
 			$this->id = (int) $id;
 			$this->usuario->setId($this->id);
 			$result = $this->usuario->buscarPorId();
 			return $result;
 		}
+		//Se hace la peticion a la clase Usuario para que retorne un objeto con todos los registros
 		public function buscarTodo(){
 			$todos = $this->usuario->buscarTodo();
 			return $todos; 
 		}
+		//Recibe todos los parametro para hacer un nuevo registro
 		public function registrar($nombre,$edad,$telefono,$correo){
 			$this->usuario->setNombre($nombre);
 			$this->usuario->setEdad($edad);
@@ -32,6 +37,7 @@ include('Models/Usuario.php');
 				return false;
 			}
 		}
+		//Apartir del id se modificara el registro
 		public function modificar($id,$nombre,$edad,$telefono,$correo){
 			$this->usuario->setId($id);
 			$this->usuario->setNombre($nombre);
@@ -44,6 +50,7 @@ include('Models/Usuario.php');
 				return false;
 			}
 		}
+		//Solamente se recibe el id del registro que se quiere eliminar
 		public function eliminar($id){
 			$this->id = (int) $id;
 			$this->usuario->setId($this->id);
